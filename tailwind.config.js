ab667df47f8a72ddc36647d2132e3f0c38d40677
raja1 <rajamr1987@gmail.com>
2025-11-12 19:01:44 +0400

    test

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.html",
+    "./js/**/*.js"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          400: '#60a5fa',
+          600: '#2563eb',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
